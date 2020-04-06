d13c64e31347717dfbd5b98e42db437acf0a3116
Nick <nsardonini@gmail.com>
2020-04-06 15:42:11 -0400

    add ::over?,  rework instance variables

--- classes.rb
+++ classes.rb
@@ -1,30 +1,18 @@
 module Classable
   class Board
-    attr_reader :code
+    attr_reader :code, :board
 
     def initialize
       @@colors = %w[red orange yellow green blue purple]
       @board = []
-      @code = []
+      @code = Array.new(4, nil)
       @feedback_holder = []
       @board_feedback = []
       create_code_cpu
     end
 
-    def colors
-      @@colors
-    end
-
     def self.board
       @board
-    end
-
-    def display_board
-      puts "\nCurrent Board:"
-      @board.each_with_index do |e,i|
-        puts "#{e.join(' ')} | #{@board_feedback[i].join(' ')}"
-      end
-      puts
     end
 
     def create_guess *colors
@@ -32,7 +20,7 @@ module Classable
       @guess_contents = colors
       @board << @guess_contents.flatten
     end
-
+    
     def feedback 
       @feedback_holder = []
       guess = @board.last
@@ -46,48 +34,59 @@ module Classable
       @board_feedback << @feedback_holder.sort!.reverse!
     end
 
+    def display_board
+      puts "\nCurrent Board:"
+      @board.each_with_index do |e,i|
+        puts "#{e.join(' ')} | #{@board_feedback[i].join(' ')}"
+      end
+      puts
+    end
+    
     private
     def create_code *colors
       @code = colors[0..3]
     end
 
     def create_code_cpu
-      4.times { @code << @@colors[rand(6)] }
+      4.times { |i| @code[i] = @@colors[rand(6)] }
     end
   end
 
   class Game < Board
-    attr_reader :board, :code
+    attr_reader :board
     
     ROUNDS = 12
-    
-    def initialize
-      super
-      @code = []
-      create_code_cpu
-    end
-
-    def self.game_start
-      game = Board.new
-      @@turn = 1
-      puts game.code
-      until Game.over?
-        game.create_guess(gets.chomp.split)
-        game.feedback
-        game.display_board
-        @@turn += 1
-        # puts "#{self.board}"
-        # puts "#{game.code}"
-      end
-    end
 
     def self.code
       @code
     end
 
+    def self.game_start
+      @game = Board.new
+      @@turn = 1
+      puts @game.code
+      @game.create_guess(gets.chomp.split)
+      until Game.over?
+        @game.feedback
+        @game.display_board
+        @@turn += 1
+        @game.create_guess(gets.chomp.split)
+      end
+      puts "\nYOU WIN!" if Game.win?
+      puts "\nYOU LOSE!" if Game.lose?
+    end
+
     def self.over?
       return true if @@turn == ROUNDS
-      #return true if @board == self.code 
+      return true if @game.board.last == @game.code 
+    end
+
+    def self.win?
+      @game.board.last == @game.code ? true : false
+    end
+
+    def self.lose?
+      @@turn == ROUNDS ? true : false
     end
   end
 end
